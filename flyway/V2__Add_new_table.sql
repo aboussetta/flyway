20c54f6b538ea2df9ee0f28082855427d97bf940
Abderrahim Boussetta <abderrahim.boussetta@performgroup.com>
2019-03-13 14:16:35 +0000

    create new tables

---
+++ flyway/V2__Add_new_table.sql
@@ -0,0 +1,13 @@
+/* Single line comment */
+CREATE TABLE car (
+  name VARCHAR(25) NOT NULL,
+  PRIMARY KEY(name)
+);
+
+/*
+Multi-line
+comment
+*/
+
+-- Placeholder
+INSERT INTO car (name) VALUES ('Renault');
