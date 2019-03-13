0c4e030c878bcceed20e4b3c507ffbdb2e73eeb5
Abderrahim Boussetta <abderrahim.boussetta@performgroup.com>
2019-03-13 14:14:52 +0000

    create new tables

---
+++ V1__Initial_create.sql
@@ -0,0 +1,19 @@
+/* Single line comment */
+CREATE TABLE test_user (
+ name VARCHAR(25) NOT NULL,
+ PRIMARY KEY(name)
+);
+
+/*
+Multi-line
+comment
+*/
+-- PL/SQL block
+CREATE TRIGGER test_trig AFTER insert ON test_user
+BEGIN
+   UPDATE test_user SET name = CONCAT(name, ' triggered');
+END;
+/
+
+-- Placeholder
+INSERT INTO test_user (name) VALUES ('Mr. T');
